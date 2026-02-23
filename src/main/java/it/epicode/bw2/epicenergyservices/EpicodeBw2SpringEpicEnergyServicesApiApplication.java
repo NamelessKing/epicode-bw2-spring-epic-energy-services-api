97864e82eee0c4cd7a5b595cf3806bccb3c6921a
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-23 11:26:48 +0100

    first commit

---
+++ src/main/java/it/epicode/bw2/epicenergyservices/EpicodeBw2SpringEpicEnergyServicesApiApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.bw2.epicenergyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeBw2SpringEpicEnergyServicesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicodeBw2SpringEpicEnergyServicesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/epicode/bw2/epicenergyservices/EpicodeBw2SpringEpicEnergyServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.bw2.epicenergyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeBw2SpringEpicEnergyServicesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
