ce4f06977188ea80bd0e9c7ccd5b20ecfd332069
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-02-24 10:36:41 +0100

    delete
    repository e service eliminate

--- src/main/java/it/epicode/bw2/epicenergyservices/dto/response/SedeCliente.java
+++ src/main/java/it/epicode/bw2/epicenergyservices/dto/response/SedeCliente.java
@@ -3,8 +3,6 @@ package it.epicode.bw2.epicenergyservices.dto.response;
 import jakarta.validation.constraints.NotNull;
 
 public record SedeCliente(long id,
-                          @NotNull(message = "Il tipo sede è obbligatorio (LEGALE o OPERATIVA)")
-                          TipoSede tipoSede,
                           @NotNull(message = "L'ID del cliente è obbligatorio")
                           Long idCliente,
                           @NotNull(message = "L'ID dell'indirizzo è obbligatorio")
--- src/main/java/it/epicode/bw2/epicenergyservices/repositories/SedeClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package it.epicode.bw2.epicenergyservices.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SedeClienteRepository extends JpaRepository<SedeCliente, Long> {
-    boolean existsByIdCliente(Long idCliente);
-}
